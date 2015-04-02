ad3f1d642180e96982fe9969a9715455ed92fc0b
Michael Schenck <schenck@hioscar.com>
2015-04-02 10:11:22 -0400

    Changed process state transitions to an incrementing gauge (instead of a counter) as these should be considered bucketed counts per time window (each window is mutually exclusive of the previous window)

--- src/main/python/apache/thermos/core/process.py
+++ src/main/python/apache/thermos/core/process.py
@@ -131,7 +131,7 @@ class ProcessBase(object):
   def _statsd_write(self, process_status):
     state_name = ProcessState._VALUES_TO_NAMES[process_status.state]
     metric_name = "%s.%s" % (process_status.process, state_name)
-    statsd_data = "%s:1|c" % metric_name
+    statsd_data = "%s:+1|g" % metric_name
     try:
       statsd_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
       statsd_sock.sendto(statsd_data, self._statsd_endpoint())
